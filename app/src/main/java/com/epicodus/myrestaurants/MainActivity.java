d366e10a1389f87c3eabcb7d515d9e5638bd18a4
Izzy George <10izzygeorge@gmail.com>
2016-04-19 17:04:08 -0700

    pass location from MainActivity to RestaurantsActibity via intent extra

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -3,6 +3,7 @@ package com.epicodus.myrestaurants;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
                 String location = mLocationEditText.getText().toString();
                 Log.d(TAG,location);
                 Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
             }
         });
