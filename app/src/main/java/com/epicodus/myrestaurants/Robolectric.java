a9890a009c16b4c57e5006c37be69423327fd7f5
10igeorge <10izzygeorge@gmail.com>
2016-04-19 21:51:07 -0700

    bind views with Butterknife

--- app/src/main/java/com/epicodus/myrestaurants/Robolectric.java
+++ app/src/main/java/com/epicodus/myrestaurants/Robolectric.java
@@ -4,6 +4,8 @@ package com.epicodus.myrestaurants;
  * Created by Guest on 4/19/16.
  */
 public class Robolectric {
+    private static Class<MainActivity> upActivity;
+
     public static void setupActivity(Class<MainActivity> upActivity) {
         Robolectric.upActivity = upActivity;
     }
